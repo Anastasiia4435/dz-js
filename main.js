3fa747d5c6e5be3d72addd7266111eb23f98aa76
Anastasiia4435 <107216310+Anastasiia4435@users.noreply.github.com>
2023-05-25 19:25:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const text1 = document.querySelector("h1");
+const text2 = document.querySelector("p");
+const text3 = document.querySelector("h2");
+
+text1.textContent = "text1";
+text2.textContent = "text2";
+text3.textContent = "text3";
+
+text1.style.color = '#df8f8f';
+text3.style.color = '#96cf70';
+text2.style.color = '#e4e182';
+
+text1.style.backgroundColor = 'black'
+text2.style.backgroundColor = 'black'
+text3.style.backgroundColor = 'black'
